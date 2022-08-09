4bd497546b2303469ef5bdbc123e6f6a7c1f0f3a
jongwon <kjw_0117@naver.com>
2022-08-09 19:11:55 +0900

    0809

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 
 function App() {
-  const [user] = useState('김종원');
+  const [user, setUser] = useState('김종원');
   const [totalMoney, setTotalMoney] = useState(1040000);
   const [inMoney, setInMoney] = useState(640000);
   const [outMoney, setOutMoney] = useState(420000);
 
   return (
-    <div className="w-full h-screen bg-blue-50 flex flex-col items-center p-2 font-nanum-regular">
+    <div className="flex flex-col items-center w-full py-4 bg-blue-50 font-nanum-regular">
       {/* 총액 및 출입금 */}
-      <div className="w-[20rem] h-[13rem] bg-blue-100 rounded-t-lg p-4 mb-1">
+      <div className="w-[20rem] h-[12rem] bg-blue-100 rounded-t-lg p-4 mb-1">
         <div>★ {user}의 가계부</div>
-        <div className="text-center h-[6.5rem] leading-[6.5rem] font-nanum-eb text-4xl">
+        <div className="text-center h-[5.5rem] leading-[5.5rem] font-nanum-eb text-4xl">
           ￦ {totalMoney}
         </div>
-        <div className="flex text-center justify-around">
+        <div className="flex justify-around text-center">
           <div className="w-[49%] bg-white py-1 rounded-md">
             들어온돈
             <br />
@@ -28,13 +28,13 @@ function App() {
         </div>
       </div>
       {/* 내역 및 추가 버튼 */}
-      <div className="w-[20rem] bg-blue-100 p-4">
-        <ul className="flex w-full border-b pb-2 border-gray-400 mb-2">
+      <div className="w-[20rem] bg-blue-100 p-4 mb-1">
+        <ul className="flex w-full pb-2 mb-2 border-b border-gray-400">
           <li>전체 내역</li>
-          <li className="text-gray-400 ml-3">수입</li>
-          <li className="text-gray-400 ml-3">지출</li>
+          <li className="ml-3 text-gray-400">수입</li>
+          <li className="ml-3 text-gray-400">지출</li>
         </ul>
-        <div className="bg-white w-full h-16 mt-1 rounded-md flex justify-between p-2">
+        <div className="flex justify-between w-full h-16 p-2 mt-1 bg-white rounded-md">
           <div>
             <span className="text-sm text-gray-400">2022-08-08</span>
             <br />
@@ -42,15 +42,67 @@ function App() {
           </div>
           <div className="leading-[3rem] text-lg mr-1 text-red-500">-12000</div>
         </div>
-        <div className="bg-white w-full h-16 mt-1 rounded-md"></div>
-        <div className="w-full h-28 bg-gray-100 mt-4 rounded-md border-dashed border border-blue-600">
-          <div className="text-center p-2">
+        <div className="w-full h-16 mt-1 bg-white rounded-md"></div>
+        <div className="w-full mt-4 bg-gray-100 border border-blue-600 border-dashed rounded-md h-28">
+          <div className="p-2 text-center cursor-pointer select-none">
             <div className="text-6xl font-nanum-bold">+</div>
             <div>새 거래 내역 추가</div>
           </div>
         </div>
       </div>
       {/* 내역 추가 */}
+      <div className="w-[20rem] bg-blue-100 p-4 rounded-b-lg">
+        <div className="pb-2 mb-2 border-b border-gray-400">새로운 거래</div>
+        <form className="flex flex-col">
+          <label className="mb-1">날짜</label>
+          <input type="date" name="" id="" className="p-2 mb-2" />
+          <label className="mb-1">내용</label>
+          <div className="mb-2">
+            <label className="box-radio-input">
+              <input
+                type="radio"
+                name="inout"
+                value="inMoney"
+                className="hidden"
+              />
+              <span className="inline-block w-[49%] px-2 mr-[1%] leading-10 text-center bg-white rounded-md cursor-pointer bg-none h-10">
+                들어온 돈
+              </span>
+            </label>
+            <label className="box-radio-input">
+              <input
+                type="radio"
+                name="inout"
+                value="outMoney"
+                className="hidden"
+              />
+              <span className="inline-block w-[49%] ml-[1%] leading-10 text-center bg-white rounded-md cursor-pointer bg-none h-10">
+                나간 돈
+              </span>
+            </label>
+          </div>
+          <input
+            type="text"
+            className="p-2 mb-2 rounded-md"
+            placeholder="메모"
+          />
+          <label className="mb-1">금액</label>
+          <input
+            type="number"
+            className="p-2 mb-2 rounded-md"
+            placeholder="얼마용"
+          />
+          <button
+            type="submit"
+            className="w-full py-3 mb-1 bg-blue-700 rounded-md"
+          >
+            추가
+          </button>
+          <button type="button" className="w-full py-3 bg-white rounded-md">
+            닫기
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
